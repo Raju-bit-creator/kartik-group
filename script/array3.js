2a4d2be45f67079461042df8394e609fc28fa082
Raju <rajutamang3456@gmail.com>
2024-11-26 16:55:11 +0545

    array methods

--- script/array3.js
+++ script/array3.js
@@ -1,4 +1,4 @@
-let cart =[
+let cart = [
     {
         price: 200,
         qty: 3
@@ -15,34 +15,34 @@ let cart =[
         price: 300,
         qty: 3
     },
-    
-  
+
+
 ]
-const total =cart.slice(0, 4).reduce((acc , curr)=> acc + curr.price * curr.qty , 0)
+const total = cart.slice(0, 4).reduce((acc, curr) => acc + curr.price * curr.qty, 0)
 console.log(" this is our total", total);
-let items= cart.slice(0, 4)
+let items = cart.slice(0, 4)
 console.log(items);
 
-let course =[
-{
-    class:"mern",
-    completeStatus: true
-},
-{
-    class:"python",
-    completeStatus: false
-},
-{
-    class:"ui/ux",
-    completeStatus: true
-},
-{
-    class:"php",
-    completeStatus: false
-}
+let course = [
+    {
+        class: "mern",
+        completeStatus: true
+    },
+    {
+        class: "python",
+        completeStatus: false
+    },
+    {
+        class: "ui/ux",
+        completeStatus: true
+    },
+    {
+        class: "php",
+        completeStatus: false
+    }
 ]
-let completed=[]
-let inComplete=[]
+let completed = []
+let inComplete = []
 
 for (let index = 0; index < course.length; index++) {
     const element = course[index];
@@ -55,18 +55,18 @@ for (let index = 0; index < course.length; index++) {
 console.log(completed);
 console.log(inComplete);
 
-let number=[1, 2, 3, 4, 5, 6, 7]
-const filteredNumber= number.filter( function (num){
-    return num> 4
+let number = [1, 2, 3, 4, 5, 6, 7]
+const filteredNumber = number.filter(function (num) {
+    return num > 4
 })
-console.log( filteredNumber);
-let number1=[1, 2, 3, 4, 5, 6, 7]
-const foundNumber= number1.find( function (num){
-    return num> 4
+console.log(filteredNumber);
+let number1 = [1, 2, 3, 4, 5, 6, 7]
+const foundNumber = number1.find(function (num) {
+    return num > 4
 })
-console.log( foundNumber);
+console.log(foundNumber);
 
-let coutries =[
+let countries = [
     {
         name: "Nepal",
         code: "Np"
@@ -85,6 +85,58 @@ let coutries =[
         code: "Jp"
     }
 ]
+//   findCountry =(name)=>{
+//    let country = countries.find(e=> e.name===name)
+//    return country ? country.code : "country not match"
+//  }
+//  let x = findCountry1("singapore")
+//  console.log(x);
+
+ function getCountryCode(countryName) {
+  
+    let country = countries.find(c => c.name.toLowerCase() === countryName.toLowerCase().trim());
+    if (country) {
+        return country.code;
+    } else {
+        return "Country not found";
+    }
+}
+
+
+console.log(getCountryCode(" nepal ")); 
+// console.log(getCountryCode("India")); 
+// console.log(getCountryCode("USA"));   
+// console.log(getCountryCode("China"));
+  
+let sentence= "he/is/a/good/boy"
+let wordSeparator = sentence.split("/")
+console.log(wordSeparator);
+console.log(wordSeparator.length);
+ 
+let num= [1, 2, 3,4]
+let newNum = num.push(5)
+console.log(newNum);
+console.log(num);
+let removeNum= num.pop()
+console.log(num);
+// assignment shift, unshift, includes, charAt method of array
+
+//this is spread operator
+let num1 =[1, 2, 3]
+let num2= [...num1 , 5, 6, 7,8] 
+console.log(num2);
+
+
+
+
+
+
+
+ 
+  
+
+    
+
 
 
 
